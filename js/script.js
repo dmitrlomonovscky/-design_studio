44fe2d3d8340d6d70278982201ab6a9758a10d2a
Dmitrii <lomonovskij@gmail.com>
2020-01-07 15:20:37 +0200

    new file

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
+
+  $(window).on("beforeunload", function () {
+    $(window).scrollTop(0);
+  });
   
+  $('a').bind("click", function(e){
+    var anchor = $(this);
+    var hh = 70; //header height
+
+    $('html, body').stop().animate({
+        scrollTop: $(anchor.attr('href')).offset().top - hh + 'px'
+    }, 750);
+    e.preventDefault();
+
+  // active item menu
+    $('a.active').removeClass('active');
+    $(this).addClass('active');
+  });
+
   $('.owl-carousel').owlCarousel({
       loop:true,
       margin:10,
